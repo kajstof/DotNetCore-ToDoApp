b6c3ce63e33533bea7b99e6fdaf3d425bc3700c9
Krzysztof Krysiak <krysiak86@gmail.com>
2017-08-01 14:58:47 +0200

    Add project files.

---
+++ DotNetCore1.1-ToDoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCore1._1_ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
