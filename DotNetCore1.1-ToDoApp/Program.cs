ef21edcea5347330d713e0c6d3dec2bfd201cb74
kajstof <krysiak86@gmail.com>
2017-08-02 22:01:25 +0200

    Refactoring

--- DotNetCore1.1-ToDoApp/Program.cs
+++ DotNetCore1.1-ToDoApp/Program.cs
@@ -3,15 +3,93 @@ using System.Collections.Generic;
 
 namespace ToDoApp
 {
+    enum ToDoAction
+    {
+        Add,
+        Remove,
+        Clear,
+        Quit,
+        Unknown
+    }
+
     class Program
     {
+        private static List<string> toDoList = new List<string>();
+
         static void Main(string[] args)
         {
-            List<string> toDoList = new List<string>();
-            // Declare
-            string option = null;
-            string taskName = null;
+            ShowConsoleApplicationHeader();
+            StartApplication();
+        }
 
+        private static void StartApplication()
+        {
+            ToDoCommand toDoCommand = null;
+            do
+            {
+                Console.Write("Your option: ");
+                toDoCommand = new ToDoCommand(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                switch (toDoCommand.Action)
+                {
+                    case ToDoAction.Add:
+                        AddItemToList(toDoCommand.TaskName);
+                        break;
+                    case ToDoAction.Remove:
+                        RemoveItemFromList(toDoCommand.TaskName);
+                        break;
+                    case ToDoAction.Clear:
+                        ClearList();
+                        break;
+                    default:
+                        UnkownOption();
+                        break;
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Items in To Do List: {{ {string.Join(", ", toDoList)} }}");
+                Console.ForegroundColor = ConsoleColor.White;
+            } while (toDoCommand.Action != ToDoAction.Quit);
+        }
+
+        private static void UnkownOption()
+        {
+            Console.WriteLine("Unknown option");
+        }
+
+        private static void ClearList()
+        {
+            toDoList.Clear();
+            Console.WriteLine("To Do List cleared");
+        }
+
+        private static void RemoveItemFromList(string taskName)
+        {
+            if (toDoList.Contains(taskName))
+            {
+                toDoList.Remove(taskName);
+                Console.WriteLine($"Item \"{taskName}\" removed from list");
+            }
+            else
+            {
+                Console.WriteLine($"Item \"{taskName}\" not exists in To Do List");
+            }
+        }
+
+        private static void AddItemToList(string taskName)
+        {
+            if (toDoList.Contains(taskName))
+            {
+                Console.WriteLine($"Item \"{taskName}\" exists in To Do List");
+            }
+            else
+            {
+                toDoList.Add(taskName);
+                Console.WriteLine($"Item \"{taskName}\" added to list");
+            }
+        }
+
+        private static void ShowConsoleApplicationHeader()
+        {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("===========================================================");
             Console.WriteLine("ToDoApp | DotNetCore 1.1 | Copyright 2017 Krzysztof Krysiak");
@@ -25,54 +103,7 @@ namespace ToDoApp
             Console.WriteLine("q        | Quit program");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("===========================================================");
-
-            while (option != "q")
-            {
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("Your option: ");
-                option = Console.ReadLine();
-                if (!(option.Length >= 2 && option.Substring(0, 2) == "--"))
-                {
-                    taskName = option.Length > 1 ? option.Substring(1) : null;
-                    option = option.Length > 0 ? option.Substring(0, 1) : null;
-                }
-
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                switch (option)
-                {
-                    case "+":
-                        if (toDoList.Contains(taskName))
-                        {
-                            Console.WriteLine($"Item \"{taskName}\" exists in To Do List");
-                        }
-                        else
-                        {
-                            toDoList.Add(taskName);
-                            Console.WriteLine($"Item \"{taskName}\" added to list");
-                        }
-                        break;
-                    case "-":
-                        if (toDoList.Contains(taskName))
-                        {
-                            toDoList.Remove(taskName);
-                            Console.WriteLine($"Item \"{taskName}\" removed from list");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Item \"{taskName}\" not exists in To Do List");
-                        }
-                        break;
-                    case "--":
-                        toDoList.Clear();
-                        Console.WriteLine("To Do List cleared");
-                        break;
-                    default:
-                        Console.WriteLine("Unknown option");
-                        break;
-                }
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Items in To Do List: {{ {string.Join(", ", toDoList)} }}");
-            }
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
---
+++ DotNetCore1.1-ToDoApp/ToDoCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp
+{
+    
+    class ToDoCommand
+    {
+        public ToDoAction Action { get; set; }
+        public string TaskName { get; set; }
+
+        public ToDoCommand(string command)
+        {
+            if (!(command.Length >= 2 && command.Substring(0, 2) == "--"))
+            {
+                TaskName = command.Length > 1 ? command.Substring(1) : null;
+                command = command.Length > 0 ? command.Substring(0, 1) : null;
+            }
+
+            switch (command)
+            {
+                case "+":
+                    Action = ToDoAction.Add;
+                    break;
+                case "-":
+                    Action = ToDoAction.Remove;
+                    break;
+                case "--":
+                    Action = ToDoAction.Clear;
+                    break;
+                case "q":
+                    Action = ToDoAction.Quit;
+                    break;
+                default:
+                    Action = ToDoAction.Unknown;
+                    break;
+            }
+        }
+    }
+}
