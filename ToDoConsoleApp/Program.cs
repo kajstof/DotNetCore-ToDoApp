5c1821e5c52bb7e8ca8a8d1878fb32461010a0c0
Krzysztof Krysiak <krysiak86@gmail.com>
2017-08-21 13:39:11 +0200

    Rename files

--- ToDoConsoleApp/Program.cs
+++ ToDoConsoleApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ToDoApp
+namespace ToDoConsoleApp
 {
     enum ToDoAction
     {
--- ToDoConsoleApp/ToDoCommand.cs
+++ ToDoConsoleApp/ToDoCommand.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ToDoApp
+namespace ToDoConsoleApp
 {
     
     class ToDoCommand
